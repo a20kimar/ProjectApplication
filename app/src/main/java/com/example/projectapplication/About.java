6fa0b0a72af090c43e8a089cee06882bec51f040
a20kimar <a20kimar@student.his.se>
2021-05-13 16:57:31 +0200

    Changed the layout for about page

--- app/src/main/java/com/example/projectapplication/About.java
+++ app/src/main/java/com/example/projectapplication/About.java
@@ -3,6 +3,7 @@ package com.example.projectapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView t = findViewById(R.id.aboutText);
+        t.setText("About\nThis application was made for a certain audience");
     }
 }
